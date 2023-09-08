d6e502bed29a5d52f0a193da90be12e99d21169c
k02sz <k02.szymecki@gmail.com>
2023-09-08 10:43:30 +0200

    gunicorn for backend deployment

--- backend/app.py
+++ backend/app.py
@@ -120,3 +120,8 @@ def delete():
     db.session.delete(todelete)
     db.session.commit()
     return('Deleted')
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
---
+++ backend/run.py
@@ -0,0 +1,8 @@
+from app import app  # Import your Flask app instance
+import os
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
+
