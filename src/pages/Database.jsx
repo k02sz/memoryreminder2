d91cf773c92ba162fd12ed94f10efa5e0a2a5792
k02sz <k02.szymecki@gmail.com>
2023-09-08 10:48:45 +0200

    backend integration

--- src/pages/Database.jsx
+++ src/pages/Database.jsx
@@ -14,7 +14,7 @@ const Database = () => {
     const odpValue = changedOdp.current.value;
     setShowModal(false)
     try {
-      const response = await fetch('https://localhost:5000/edit', {
+      const response = await fetch('http://localhost:5000/edit', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -37,7 +37,7 @@ const Database = () => {
   const handleClickDelete = async () => {
     setShowModal(false)
     try {
-      const response = await fetch('https://localhost:5000/delete', {
+      const response = await fetch('http://localhost:5000/delete', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -64,7 +64,7 @@ const Database = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch('https://localhost:5000/all');
+      const response = await fetch('http://localhost:5000/all');
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
