ae329f07d1f6b85583e33d9f98786ce196352e30
k02sz <k02.szymecki@gmail.com>
2023-09-10 10:56:37 +0200

    Update Addword.jsx

--- src/pages/Addword.jsx
+++ src/pages/Addword.jsx
@@ -41,13 +41,15 @@ const Addword = () => {
             <input ref={newOdp} type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         </div>
         <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
-            <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={handleClick}
-            >
-                Add
-            </button>
+            <Link to="/">
+              <button
+                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={handleClick}
+              >
+                  Add
+              </button>
+            </Link>
         </div>
     </div>
   )
